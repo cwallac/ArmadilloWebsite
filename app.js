a7a66fd63d5fb2375c3002284a4c16a784f48e71
cwallac <christopher.wallace@students.olin.edu>
2014-04-02 13:50:06 -0400

    Fixed accidental file deletions

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 var path = require('path'),
 	express = require('express'),
 	app = express(),
-	download = require('./download'),
+	
 	url = require('url');
 
 
 
 //Renders homepage
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-app.get('/team.html', function(req, res) {
-  res.sendfile(__dirname + '/team.html');
-});
-
-app.get('/product.html', function(req, res) {
-  res.sendfile(__dirname + '/product.html');
-});
-
-app.get('/feedback.html', function(req, res) {
-  res.sendfile(__dirname + '/feedback.html');
+app.get('*', function(req, res) {
+	var site = url.parse(req.url,true).pathname
+  res.sendfile(__dirname + '/Public' + site);
 });
 
 
